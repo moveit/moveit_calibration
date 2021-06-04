c14982df8caa38fb9b923280568836f342d45cca
John Stechschulte <john@picknik.ai>
2021-06-04 13:50:46 -0600

    implemented reproj err

--- moveit_calibration_plugins/handeye_calibration_solver/include/moveit/handeye_calibration_solver/handeye_solver_base.h
+++ moveit_calibration_plugins/handeye_calibration_solver/include/moveit/handeye_calibration_solver/handeye_solver_base.h
@@ -37,6 +37,7 @@
 #pragma once
 
 #include <tf2_eigen/tf2_eigen.h>
+#include <ros/console.h>
 
 namespace moveit_handeye_calibration
 {
@@ -82,6 +83,64 @@ public:
    * @return A 4X4 transform indicating the pose.
    */
   virtual const Eigen::Isometry3d& getCameraRobotPose() const = 0;
+
+  /**
+   * @brief Get the reprojection error for the given samples.
+   * @param effector_wrt_world End-effector pose (4X4 transform) with respect to
+   * the world (or robot base).
+   * @param object_wrt_sensor Object (calibration board) pose (4X4 transform)
+   * with respect to the camera.
+   * @param X The calibration, as a 4X4 transform.
+   * @param setup Camera mount type, {EYE_TO_HAND, EYE_IN_HAND}.
+   * @return Pair of translation and rotation reprojection error in meters and radians, or NaNs on error.
+   */
+  std::pair<double, double> getReprojectionError(const std::vector<Eigen::Isometry3d>& effector_wrt_world,
+                                                 const std::vector<Eigen::Isometry3d>& object_wrt_sensor,
+                                                 const Eigen::Isometry3d& X, SensorMountType setup = EYE_TO_HAND)
+  {
+    auto ret = std::make_pair(std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN());
+    if (effector_wrt_world.size() != object_wrt_sensor.size())
+    {
+      ROS_ERROR_NAMED("moveit_calibration_handeye_solver",
+                      "Different number of optical and kinematic transforms when calculating reprojection error.");
+      return ret;
+    }
+
+    if (effector_wrt_world.empty())
+    {
+      return ret;
+    }
+
+    double rotation_err = 0;
+    double translation_err = 0;
+
+    const size_t num_motions = effector_wrt_world.size() - 1;
+    for (size_t i = 0; i < num_motions; ++i)
+    {
+      // Calculate both sides of AX = XB
+      Eigen::Isometry3d A;
+      if (setup == EYE_IN_HAND)
+        A = effector_wrt_world[i].inverse() * effector_wrt_world[i + 1];
+      else
+        A = effector_wrt_world[i] * effector_wrt_world[i + 1].inverse();
+      Eigen::Isometry3d B = object_wrt_sensor[i] * object_wrt_sensor[i + 1].inverse();
+
+      Eigen::Isometry3d AX = A * X;
+      Eigen::Isometry3d XB = X * B;
+
+      // Rotation error
+      double r_err = Eigen::AngleAxisd(AX.rotation().transpose() * XB.rotation()).angle();
+      rotation_err += r_err * r_err;
+
+      // Translation error
+      double t_err = ((AX.translation() - XB.translation()).norm() +
+                      (AX.inverse().translation() - XB.inverse().translation()).norm()) /
+                     2.;
+      translation_err += t_err * t_err;
+    }
+    ret = std::make_pair(std::sqrt(rotation_err / num_motions), std::sqrt(translation_err / num_motions));
+    return ret;
+  }
 };
 
 }  // namespace moveit_handeye_calibration
